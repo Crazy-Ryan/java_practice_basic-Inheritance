fafc088a1abaf414a8f8450f9869b65c8ec16f2a
Ryan.Wang <ranwang1995@outlook.com>
2020-02-13 20:52:08 +0800

    [WangRan] feat: add class Animal

---
+++ 005_extends/src/main/java/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    int age;
+    int weight;
+
+    public void eat() {
+        System.out.println("I am eating");
+    }
+
+    public void sleep() {
+        System.out.println("zzzZZZ");
+    }
+}
