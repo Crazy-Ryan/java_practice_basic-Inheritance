ac5d298f447eeee5565c417d5b27a972f2b8dec1
Ryan.Wang <ranwang1995@outlook.com>
2020-02-13 20:55:17 +0800

    [WangRan] feat: add getter, setter and constructor to class Animal

--- 005_extends/src/main/java/Animal.java
+++ 005_extends/src/main/java/Animal.java
@@ -1,6 +1,30 @@
 public class Animal {
-    int age;
-    int weight;
+    private int age;
+    private int weight;
+
+    public Animal() {
+    }
+
+    public Animal(int age, int weight) {
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 
     public void eat() {
         System.out.println("I am eating");
